5183e26258a38fec0fe424ca24dcc127723056d3
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2023-11-21 23:01:43 +0800

    remove histfile

--- shellrc/histfile.zsh
+++ shellrc/histfile.zsh
@@ -1,249 +1,164 @@
 
-: 1700378374:0;vim histfile.zsh
-: 1700378378:0;cd ..
-: 1700378391:0;git add . && git commit -m "configure gBar" && git push
-: 1700378455:0;vim README.md
-: 1700378741:0;cd ~/rices/dots-hyprland
-: 1700378744:0;vim README.md
-: 1700378948:0;cd ..
-: 1700378954:0;vim aylur_dotfile/README.md
-: 1700379744:0;sudo pacman -S flameshot
-: 1700379754:0;flameshot
-: 1700379783:0;sudo pacman -S grim
-: 1700379787:0;flameshot
-: 1700379832:0;vim ~/.config/hypr/configs/env.conf
-: 1700379880:0;flameshot
-: 1700379899:0;sudo pacman -Rns flameshot grim
-: 1700380168:0;sudo pacman -S slurp grim
-: 1700380184:0;cd .config/scripts
-: 1700380192:0;vim screenshot
-: 1700380332:0;cp screenshot wayscreenshot
-: 1700380336:0;vim screenshot
-: 1700380471:0;vim ../hypr/configs/keybinds.conf
-: 1700380490:0;cd .config/scripts
-: 1700380534:0;Hyprland
-: 1700380548:0;~/.config/scripts/screenshot
-: 1700380567:0;mkdir -P ~/Pictures/screenshots
-: 1700380571:0;mkdir -p ~/Pictures/screenshots
-: 1700380587:0;~/.config/scripts/screenshot
-: 1700380607:0;vim .config/scripts/screenshot
-: 1700380808:0;slurp
-: 1700380834:0;vim .config/scripts/screenshot
-: 1700380979:0;cd .config/scripts
-: 1700380989:0;vim wayscreenshot
-: 1700381397:0;vim .config/scripts/screenshot
-: 1700381494:0;~/.config/scripts/screenshot full
-: 1700381503:0;~/.config/scripts/screenshot area
-: 1700381631:0;vim .config/hypr/configs/keybinds.conf
-: 1700381643:0;Hyprland
-: 1700381741:0;rm -rf 20231119_1*
-: 1700381747:0;cd Pictures/screenshots
-: 1700381755:0;rm -rf ./Sun-Nov-19-0*
-: 1700381757:0;reboot
-: 1700381765:0;upgrade
-: 1700381770:0;reboot
-: 1700381797:0;Hyprland
-: 1700381817:0;fetch
-: 1700381940:0;vim .config/fastfetch/kelen.jsonc
-: 1700381956:0;fetch
-: 1700382028:0;cd dotfile
-: 1700382037:0;cd rices/dots-hyprland
-: 1700382044:0;mkdir assets
-: 1700382052:0;cp ~/Pictures/screenshots/Sun-Nov-19-04:19:57-PM-HKT-2023.png ./
-: 1700382066:0;mv Sun-Nov-19-04:19:57-PM-HKT-2023.png assets
-: 1700382068:0;cd assets
-: 1700382091:0;mv Sun-Nov-19-04:19:57-PM-HKT-2023.png desktop.png
-: 1700382102:0;vim README.md
-: 1700382107:0;cd ..
-: 1700382109:0;vim README.md
-: 1700382142:0;fetch
-: 1700382150:0;wttr
-: 1700382159:0;fetch
-: 1700382268:0;git add . && git commit -m "update readme" && git push
-: 1700382305:0;fetch
-: 1700382309:0;wttr
-: 1700382317:0;fetch
-: 1700382443:0;wttr
-: 1700382454:0;cd dotfile
-: 1700382458:0;vim README.md
-: 1700382496:0;cd .config/scripts
-: 1700382538:0;chmod a+x agsloads.sh
-: 1700382541:0;fetch
-: 1700382612:0;cp ~/Pictures/screenshots/Sun-Nov-19-04:27:54-PM-HKT-2023.png ~/dotfile/assets/window.png
-: 1700382617:0;cd ~/dotfile
-: 1700382624:0;vim README.md
-: 1700382648:0;git add . && git commit -m "update readme" && git push
-: 1700382740:0;fetch
-: 1700382783:0;cp ~/Pictures/screenshots/Sun-Nov-19-04:32:37-PM-HKT-2023.png ~/dotfile/assets/window.png
-: 1700382790:0;git add . && git commit -m "update readme" && git push
-: 1700382823:0;cd assets
-: 1700382827:0;n
-: 1700382837:0;git add . && git commit -m "update readme" && git push
-: 1700382901:0;cd ..
-: 1700382905:0;git add .
-: 1700382911:0;git add . && git commit -m "update readme" && git push
-: 1700382939:0;vim README.md
-: 1700382946:0;cd assets
-: 1700382951:0;mv window.png windows.png
-: 1700382953:0;cd ..
-: 1700382955:0;git add . && git commit -m "update readme" && git push
-: 1700383195:0;cd ~
-: 1700383198:0;cd rices
-: 1700383215:0;git clone https://github.com/Tail-R/Tail-R
-: 1700383306:0;systemctl start v2raya v2ray
-: 1700383312:0;git clone https://github.com/Tail-R/Tail-R
-: 1700383325:0;git clone https://github.com/Tail-R/xmonad_eww_dotfiles
-: 1700383689:0;git clone https://github.com/yingchangliu/yingchangliu
-: 1700383694:0;cd yingchangliu
-: 1700383705:0;cp ../Tail-R/README.md ./
-: 1700383706:0;vim README.md
-: 1700384308:0;cd ..
-: 1700384312:0;rm -rf yingchangliu
-: 1700384493:0;vim ~/.config/gBar/config
-: 1700384517:0;gBar bar 0
-: 1700384525:0;killall gBar
-: 1700384526:0;gBar bar 0
-: 1700384539:0;vim ~/.config/gBar/config
-: 1700384544:0;gBar bar 0
-: 1700384546:0;vim ~/.config/gBar/config
-: 1700384550:0;gBar bar 0
-: 1700384577:0;Hyprland
-: 1700384584:0;fetch
-: 1700384590:0;wttr
-: 1700384631:0;fetchch
-: 1700384633:0;fetch
-: 1700384645:0;vim dotfile/README.md
-: 1700384674:0;fetch
-: 1700384717:0;cd dotfile
-: 1700384719:0;cd assets
-: 1700384730:0;cp ~/Pictures/screenshots/Sun-Nov-19-05:05:09-PM-HKT-2023.png ./windows.png
-: 1700384733:0;cd ..
-: 1700384736:0;git add . && git commit -m "update readme" && git push
-: 1700384765:0;vim .gitignore
-: 1700387693:0;pacman -Ss discord
-: 1700387717:0;sudo pacman -S discord
-: 1700387730:0;discord
-: 1700391103:0;cd dotfile
-: 1700391106:0;vim README.md
-: 1700391537:0;cd ~
-: 1700391936:0;cd dotfile
-: 1700391937:0;git add . && git commit -m "update readme" && git push
-: 1700396656:0;upgrade
-: 1700396681:0;aurupgrade
-: 1700396997:0;reboot
-: 1700397903:0;Hyprland
-: 1700398927:0;systemctl start v2raya v2ray
-: 1700402599:0;upgrade
-: 1700403415:0;cleanup
-: 1700407641:0;fetch
-: 1700407649:0;sfaddimag
-: 1700410689:0;upgrade
-: 1700410698:0;aurupgrade
-: 1700410735:0;vim ~/.config/autostart.sh
-: 1700410761:0;poweroff
-: 1700442932:0;Hyprland
-: 1700443249:0;upgrade
-: 1700443257:0;aurupgrade
-: 1700443282:0;reboot
-: 1700443594:0;Hyprland
-: 1700446321:0;cd /opt/madagascar-git
-: 1700446325:0;cd src
-: 1700447131:0;cd api/c
-: 1700447141:0;less blas.c
-: 1700449682:0;cd ../
-: 1700449691:0;cd ../user
-: 1700449707:0;cd seisinv
-: 1700449712:0;less pwd.c
-: 1700449727:0;../
-: 1700449730:0;cd ..
-: 1700449736:0;cd chengjb
-: 1700449895:0;less Mvti2dpseudoplrsep1.cc
-: 1700451802:0;cd ~/dotfile/shellrc
-: 1700451808:0;vim zshplugin.zsh
-: 1700451829:0;cd ~/dotfile
-: 1700451853:0;git add . && git commit -m "fix zshplugin when there is not kitty installed" && git push
-: 1700457631:0;upgrade
-: 1700457635:0;aurupgrade
-: 1700461470:0;systemctl start v2raya v2ray
-: 1700471941:0;ping www.baidu.com
-: 1700472005:0;Hyprland
-: 1700472020:0;upgrade
-: 1700472028:0;aurupgrade
-: 1700472085:0;sudo pacman -Rns eww
-: 1700472093:0;sudo pacman -Rns eww-tray-wayland-git
-: 1700472107:0;sudo pacman -Qq
-: 1700472263:0;systemctl start v2raya v2ray
-: 1700484838:0;upgrade
-: 1700484843:0;aurupgrade
-: 1700493420:0;poweroff
-: 1700533382:0;Hyprland
-: 1700533436:0;sfkirchinv
-: 1700533455:0;sfkirmig
-: 1700533705:0;cd /opt/madagascar-git/src/user/llisiw
-: 1700533758:0;vim Mkirmig.c
-: 1700533788:0;vim kirmig.c
-: 1700533822:0;vim Mkirmig.c
-: 1700533922:0;vim Mkirmig0.c
-: 1700533962:0;sfkirchnew
-: 1700533983:0;sfkirmigsr
-: 1700534007:0;vim Mkirmigsr.c
-: 1700534069:0;cd /opt/madagascar-git/src/user/llisiw
-: 1700534078:0;cd ..
-: 1700534087:0;cd ../../include
-: 1700534097:0;cd ..
-: 1700545538:0;upgrade
-: 1700545550:0;aurupgrade
-: 1700545685:0;paru -Ss aylur-gtk
-: 1700545691:0;paru -Ss aylurs-gtk
-: 1700545702:0;paru -S aylurs-git-shell
-: 1700545710:0;paru -S aylurs-gtk-shell
-: 1700545729:0;systemctl start v2raya v2ray
-: 1700545742:0;paru -S aylurs-gtk-shell
-: 1700545777:0;reboot
-: 1700545805:0;Hyprland
-: 1700545816:0;cd opt
-: 1700545819:0;cd madagascar-git
-: 1700545821:0;vim PKGBUILD
-: 1700545852:0;git show
-: 1700545879:0;makepkg --printsrcinfo
-: 1700545906:0;vim .SRCINFO
-: 1700546056:0;makepkg --printsrcinfo > .SRCINFO
-: 1700546082:0;git add PKGBUILD .SRCINFO
-: 1700546089:0;git show
-: 1700546174:0;git commit "the path fix patch has been merged upstream"
-: 1700546207:0;git commit -m "the path fix patch has been merged upstream"
-: 1700546210:0;git show
-: 1700546230:0;git push
-: 1700546250:0;cd ~/dotfile/pkgbuilds
-: 1700546271:0;cp -r ~/opt/madagascar-git/PKGBUILD ~/opt/madagascar-git/.SRCINFO ./maintain/madagascar-git
-: 1700546277:0;cd ~/dotfile
-: 1700546294:0;git add . && git commit -m "update pkgbuild of madagascar-git" && git push
-: 1700548664:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700548671:0;Hyprland
-: 1700548679:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700548685:0;Hyprland
-: 1700548689:0;fetch
-: 1700548704:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700548711:0;Hyprland
-: 1700548714:0;fetch
-: 1700548734:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700548740:0;Hyprland
-: 1700548744:0;fetch
-: 1700548752:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700548758:0;Hyprland
-: 1700548765:0;fetch
-: 1700550705:0;wttr
-: 1700550710:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700550773:0;Hyprland
-: 1700550790:0;fetch
-: 1700550811:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700550820:0;Hyprland
-: 1700550839:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700550846:0;Hyprland
-: 1700550851:0;fetch
-: 1700550974:0;vim ~/.config/hypr/configs/monitors.conf
-: 1700550982:0;Hyprland
-: 1700550987:0;fetch
-: 1700551330:0;vim ~/.config/conky/wlconkyrc
-: 1700551608:0;cd ~/dotfile
-: 1700551629:0;git add . && git commit -m "115% display scaling" && git push
+vim histfile.zsh
+git add . && git commit -m "configure gBar" && git push
+cd ~/rices/dots-hyprland
+vim aylur_dotfile/README.md
+sudo pacman -S flameshot
+sudo pacman -S grim
+vim ~/.config/hypr/configs/env.conf
+flameshot
+sudo pacman -Rns flameshot grim
+sudo pacman -S slurp grim
+cp screenshot wayscreenshot
+vim screenshot
+vim ../hypr/configs/keybinds.conf
+mkdir -P ~/Pictures/screenshots
+mkdir -p ~/Pictures/screenshots
+~/.config/scripts/screenshot
+slurp
+vim wayscreenshot
+vim .config/scripts/screenshot
+~/.config/scripts/screenshot full
+~/.config/scripts/screenshot area
+vim .config/hypr/configs/keybinds.conf
+rm -rf 20231119_1*
+cd Pictures/screenshots
+rm -rf ./Sun-Nov-19-0*
+vim .config/fastfetch/kelen.jsonc
+cd rices/dots-hyprland
+mkdir assets
+cp ~/Pictures/screenshots/Sun-Nov-19-04:19:57-PM-HKT-2023.png ./
+mv Sun-Nov-19-04:19:57-PM-HKT-2023.png assets
+mv Sun-Nov-19-04:19:57-PM-HKT-2023.png desktop.png
+cd .config/scripts
+chmod a+x agsloads.sh
+cp ~/Pictures/screenshots/Sun-Nov-19-04:27:54-PM-HKT-2023.png ~/dotfile/assets/window.png
+cp ~/Pictures/screenshots/Sun-Nov-19-04:32:37-PM-HKT-2023.png ~/dotfile/assets/window.png
+n
+git add .
+mv window.png windows.png
+cd rices
+git clone https://github.com/Tail-R/Tail-R
+git clone https://github.com/Tail-R/xmonad_eww_dotfiles
+git clone https://github.com/yingchangliu/yingchangliu
+cd yingchangliu
+cp ../Tail-R/README.md ./
+rm -rf yingchangliu
+killall gBar
+vim ~/.config/gBar/config
+gBar bar 0
+fetchch
+vim dotfile/README.md
+cd assets
+cp ~/Pictures/screenshots/Sun-Nov-19-05:05:09-PM-HKT-2023.png ./windows.png
+vim .gitignore
+pacman -Ss discord
+sudo pacman -S discord
+discord
+vim README.md
+cd dotfile
+git add . && git commit -m "update readme" && git push
+cleanup
+sfaddimag
+vim ~/.config/autostart.sh
+cd src
+cd api/c
+less blas.c
+cd ../
+cd ../user
+cd seisinv
+less pwd.c
+../
+cd chengjb
+less Mvti2dpseudoplrsep1.cc
+cd ~/dotfile/shellrc
+vim zshplugin.zsh
+git add . && git commit -m "fix zshplugin when there is not kitty installed" && git push
+ping www.baidu.com
+sudo pacman -Rns eww
+sudo pacman -Rns eww-tray-wayland-git
+sudo pacman -Qq
+poweroff
+sfkirchinv
+sfkirmig
+vim kirmig.c
+vim Mkirmig.c
+vim Mkirmig0.c
+sfkirchnew
+sfkirmigsr
+vim Mkirmigsr.c
+cd ../../include
+cd ..
+paru -Ss aylur-gtk
+paru -Ss aylurs-gtk
+paru -S aylurs-git-shell
+paru -S aylurs-gtk-shell
+cd opt
+cd madagascar-git
+vim PKGBUILD
+makepkg --printsrcinfo
+vim .SRCINFO
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git commit "the path fix patch has been merged upstream"
+git commit -m "the path fix patch has been merged upstream"
+git show
+git push
+cd ~/dotfile/pkgbuilds
+cp -r ~/opt/madagascar-git/PKGBUILD ~/opt/madagascar-git/.SRCINFO ./maintain/madagascar-git
+git add . && git commit -m "update pkgbuild of madagascar-git" && git push
+vim ~/.config/hypr/configs/monitors.conf
+vim ~/.config/conky/wlconkyrc
+cd ~/dotfile
+git add . && git commit -m "115% display scaling" && git push
+vim dotfile/shellrc/aliasrc.sh
+less ~/dotfile/shellrc/history.zsh
+less ~/dotfile/shellrc/histfile.zsh
+aurupgrade
+vim dotfile/config/conky/wlconkyrc
+hyprctl layer
+hyprctl layers
+killall swaybg
+sudo pacman -S swww
+sudo paru -S swww
+cd .config
+cd /opt/madagascar-git
+cd ~
+pacman -Ss hyprland
+cd ~/.cache/paru/clone
+rm -rf hyprland-nvidia-git
+paru -S aur/hyprland-nvidia-git
+sudo pacman -S hyprland
+sudo pacman -S hyprland-git
+upgrade
+reboot
+paru -S aur/hyprland-git
+systemctl start v2raya v2ray
+sudo pacman -S hyprland-nvidia-hidpi-git
+sudo pacman -S hyprland-nvidia-git
+wttr
+fetch
+cd /opt/madagascar-git/src/user/llisiw
+less cpxeiko.c
+killall conky
+conky -c ~/.config/conky/wlconkyrc
+Hyprland
+vim dotfile/config/hypr/configs/monitors.conf
+vim .config/conky/wlconkyrc
+exit
+: 1700576379:0;Hyprland
+: 1700576387:0;vim dotfile/config/hypr/configs/monitors.conf
+: 1700576399:0;killall conky
+: 1700576401:0;conky -c ~/.config/conky/wlconkyrc
+: 1700576408:0;vim dotfile/config/hypr/configs/monitors.conf
+: 1700576413:0;vim .config/conky/wlconkyrc
+: 1700576425:0;reboot
+: 1700576455:0;Hyprland
+: 1700578040:0;systemctl start v2raya v2ray
+: 1700578759:0;cd
+: 1700578765:0;mkdir rockstory
+: 1700578767:0;cd rockstory
+: 1700578782:0;git clone https://github.com/rock-story/rsfwi
+: 1700578800:0;cd ../dotfile
+: 1700578806:0;vim .gitignore
+: 1700578895:0;git add .
