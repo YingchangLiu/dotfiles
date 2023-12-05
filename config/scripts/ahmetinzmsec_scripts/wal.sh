64da46ff88c45371ad13c1e5c18fc20cae5ab042
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2023-12-05 13:57:49 +0800

    scripts

--- config/scripts/ahmetinzmsec_scripts/wal.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x "$(command -v feh)" ]; then
-  feh --randomize --bg-fill $HOME/.config/hypr/backgrounds/*
-fi
--- config/scripts/ahmetinzmsec_scripts/wall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x "$(command -v feh)" ]; then
-  feh --randomize --bg-fill $HOME/Insync/Apps/Desktoppr/*
-fi
-
