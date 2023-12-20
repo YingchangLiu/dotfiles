e60c569e7db52eab0e67f15a445a828ae043185c
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2023-12-21 01:56:59 +0800

    set en_US in terminal

--- shellrc/language.sh
+++ shellrc/language.sh
@@ -1,7 +1,9 @@
 # Set language to English if in graphical environment
 if [[ -n "$DISPLAY" ]]; then
-    export LANG=en_US.UTF-8
-    # A English locale is required for some programs to work properly.
-    # When in Chinese locale, terminal may not display some characters correctly.
-    # export LC_ALL=en_US.UTF-8
+    if [[ "$TERM" =~ xterm ]]; then
+        export LANG=en_US.UTF-8
+        # A English locale is required for some programs to work properly.
+        # When in Chinese locale, terminal may not display some characters correctly.
+        # export LC_ALL=en_US.UTF-8
+    fi
 fi
