0925551922b4a60d5aa8665e2a2a0e30677087ba
YingchangLiu <58721349+YingchangLiu@users.noreply.github.com>
2023-12-21 13:40:38 +0800

    add intel oneapi path

--- shellrc/path.sh
+++ shellrc/path.sh
@@ -10,10 +10,7 @@ add_path() {
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
 export NVCC_PREPEND_FLAGS='-ccbin /usr/local/cuda/bin'
 add_path /usr/local/cuda/bin
-## Enable conda in command line
-source /opt/miniconda/etc/profile.d/conda.sh 2>/dev/null
-source /opt/conda/etc/profile.d/conda.sh 2>/dev/null
-source ~/opt/miniconda3/etc/profile.d/conda.sh 2>/dev/null
+
 
 ## Seisunix   madagascar   sac  and other geosoftwares
 # export OPTROOT='/opt'
@@ -28,7 +25,6 @@ export SOFI3DROOT="${OPTROOT}/SOFI3D"
 export IFOS2DROOT="${OPTROOT}/IFOS2D"
 export IFOS3DROOT="${OPTROOT}/IFOS3D"
 
-source $RSFROOT/share/madagascar/etc/env.sh 2>/dev/null
 
 # export PATH="${PATH}:${CWPROOT}/bin:${SACROOT}/bin:${RSFROOT}/bin" 2>/dev/null
 # export PATH="$PATH:${SOFI2DROOT}/bin:${SOFI3DROOT}/bin:${IFOS2DROOT}/bin:${IFOS3DROOT}/bin" 2>/dev/null
@@ -52,3 +48,11 @@ add_path ~/dotfile/script
 #         add_path "$dir"
 #     fi
 # done
+## Enable conda in command line
+
+source /opt/miniconda/etc/profile.d/conda.sh 2>/dev/null
+source /opt/conda/etc/profile.d/conda.sh 2>/dev/null
+source ~/opt/miniconda3/etc/profile.d/conda.sh 2>/dev/null
+
+source $RSFROOT/share/madagascar/etc/env.sh 2>/dev/null
+source /opt/intel/opeapi/setvars.sh >/dev/null 2>&1
