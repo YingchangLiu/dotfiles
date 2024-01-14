825bdd4ca7dae9846b3811c05f0cdcc17a67d536
kelen51215 <58721349+kelen51215@users.noreply.github.com>
2024-01-14 22:27:01 +0800

    update environment.sh and separate Display variables

--- shellrc/environment.sh
+++ shellrc/environment.sh
@@ -1,3 +1,6 @@
+## Note that Display Environment Variables are not set here, it should be set in Window Manager's startup script.
+## For example, Hyprland loads ~/.config/hypr/configs/env.conf first.
+
 export NNN_FIFO=/tmp/nnn.fifo
 export TMPDIR=/tmp
 
@@ -7,4 +10,15 @@ NNN_PLUG_DEFAULT='b:boom;p:preview-tui;j:autojump;v:imgview;t:nmount;d:diffs;f:f
 NNN_PLUG_INLINE='E:!go run "$nnn"*'
 NNN_PLUG="$NNN_PLUG_DEFAULT;$NNN_PLUG_INLINE"
 export NNN_PLUG
+
+
+
+## Input method
+export INPUT_METHOD=fcitx
+export GTK_IM_MODULE=
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=fcitx
+export IMSETTINGS_MODULE=fcitx
 
