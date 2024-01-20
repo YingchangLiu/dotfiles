9c57b79264a0fea61b7926cfa3480fb91b6f39bb
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2024-01-20 13:57:17 +0800

    add wfrecord scripts

--- config/scripts/wfrecord-start.sh
+++ config/scripts/wfrecord-start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
-wf-recorder -f $HOME/Videos/$(date -Is).mp4 &
+geometry=$(slurp)
+if [ $? -ne 0 ]; then
+    echo "Selection cancelled"
+    exit 1
+fi
+wf-recorder -g "$(geometry)" -f ~/Videos/recording_$(date +"%Y-%m-%d_%H:%M:%S.mp4") &
 notify-send '  Screen cap started. Screen recording...'
