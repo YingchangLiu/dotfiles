2671225c4dbb78196a3d4fabfea29af18e8494f7
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2024-02-15 18:58:21 +0800

    add bottom waybar config

--- shellrc/language.sh
+++ shellrc/language.sh
@@ -2,8 +2,8 @@
 if [[ -n "$DISPLAY" ]]; then
     if [[ "$TERM" =~ xterm ]]; then
         export LANG=en_US.UTF-8
-        # A English locale is required for some programs to work properly.
-        # When in Chinese locale, terminal may not display some characters correctly.
-        export LC_ALL=zh_CN.UTF-8
+        ## A English locale is required for some programs to work properly.
+        ## When in Chinese locale, terminal may not display some characters correctly.
+        #export LC_ALL=zh_CN.UTF-8
     fi
 fi
