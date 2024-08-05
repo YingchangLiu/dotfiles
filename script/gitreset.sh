5a3317529ccf82fe4e305aac70b98f12913bf911
kelen51215 <58721349+kelen51215@users.noreply.github.com>
2024-08-05 21:03:59 +0800

    update gitshrink.sh and install_dots.sh

--- script/gitreset.sh
+++ script/gitreset.sh
@@ -1,6 +1,7 @@
 git checkout --orphan tmp
 git add -A
-git commit -m "clean all commits, then build a new branch"
+git commit -m "New root commit"
 git branch -D master
 git branch -m master
 git push -f origin master
+git gc --aggressive --prune=now
