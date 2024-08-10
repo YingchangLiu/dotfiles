5d1b141f190c9f16ee718c65438c72695f6a0b13
kelen51215 <58721349+kelen51215@users.noreply.github.com>
2024-08-10 20:48:31 +0800

    move zsh plugins to extra/

--- config/zsh-autocomplete/run-tests.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh -f
-cd $( git rev-parse --show-toplevel )
-
-git --version
-typeset -p1 PWD VENDOR OSTYPE =zsh ZSH_VERSION ZSH_PATCHLEVEL
-env -i HOME=$( mktemp -d ) PATH=$PATH FPATH=$FPATH zsh -f -- \
-    =clitest --list-run --progress dot --prompt '%' --color always \
-        -- $PWD/Tests/*.md
--- shellrc/zshplugin.zsh
+++ shellrc/zshplugin.zsh
@@ -5,23 +5,25 @@ case $DISTRO in
     source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme 2>/dev/null
     source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 2>/dev/null
     source /usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh 2>/dev/null && _autocomplete=true
-    source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh 2>/dev/null
     source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
+    source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh 2>/dev/null
     # You should install "pkgfile" and exec 'sudo pkgfile -u' in archlinux or "command-not-found" in debian to use the script.
     source /usr/share/doc/pkgfile/command-not-found.zsh 2>/dev/null
     ;;
     *Debian*|*debian*)
     source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh 2>/dev/null
-    source $HOME/.config/powerlevel10k/powerlevel10k.zsh-theme 2>/dev/null
     source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
+    source $HOME/.local/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme 2>/dev/null
+    source $HOME/.local/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh 2>/dev/null && _autocomplete=true
+    # source $HOME/.local/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh 2>/dev/null
     . /etc/zsh_command_not_found 2>/dev/null  # need command-not-found
     ;;
     *Gentoo*|*gentoo*)
     source /usr/share/zsh/site-functions/powerlevel10k/powerlevel10k.zsh-theme 2>/dev/null
     source /usr/share/zsh/site-functions/zsh-autocomplete/zsh-autocomplete.plugin.zsh2>/dev/null && _autocomplete=true
     source /usr/share/zsh/site-functions/zsh-autosuggestions.zsh 2>/dev/null
-    source /usr/share/zsh/site-functions/zsh-history-substring-search.zsh 2>/dev/null
     source /usr/share/zsh/site-functions/zsh-syntax-highlighting.zsh 2>/dev/null
+    source /usr/share/zsh/site-functions/zsh-history-substring-search.zsh 2>/dev/null
     ;;
 # else
 #     echo "This is not Arch Linux / Debian or Gentoo. You need fix the plugin path for your distrobution."
