d5c754e7bab8760b0d53158c0df6e995b2782f7a
YingchangLiu <58721349+YingchangLiu@users.noreply.github.com>
2024-08-12 03:50:50 +0800

    fix rsf path

--- shellrc/path.sh
+++ shellrc/path.sh
@@ -1,5 +1,30 @@
 ##  add directories to PATH.
 
+## Load madagascar environment
+[ -z "$_LOADED_RSF_ENV" ] && source $RSFROOT/share/madagascar/etc/env.sh 2>/dev/null && export _LOADED_RSF_ENV=1
+
+## Enable conda in command line
+set_conda_env() {
+    local __conda_optpaths=(
+        "/opt/miniconda"
+        "/opt/conda"
+        "$OPTROOT/miniconda"
+        "$OPTROOT/miniconda3"
+        "$HOME/opt/miniconda3"
+    )
+    local conda_path
+    for conda_path in "${__conda_optpaths[@]}"; do
+        if [ -d "$conda_path" ]; then
+            [ -z "$_LOADED_CONDA_ENV" ] && source "$conda_path/etc/profile.d/conda.sh" 2>/dev/null && export _LOADED_CONDA_ENV=1
+            break
+        fi
+    done
+}
+set_conda_env
+
+
+# source /opt/intel/oneapi/setvars.sh >/dev/null 2>&1
+
 
 ## CUDA
 set_ld_library_path /usr/local/cuda/lib64 /usr/local/cuda/extras/CUPTI/lib64
@@ -20,8 +45,9 @@ export SOFI3DROOT="${SOFI3DROOT:-${OPTROOT}/SOFI3D}"
 export IFOS2DROOT="${IFOS2DROOT:-${OPTROOT}/IFOS2D}"
 export IFOS3DROOT="${IFOS3DROOT:-${OPTROOT}/IFOS3D}"
 
+## set_path of rsf is not needed, because it is already set in env.sh
+# set_path $RSFROOT/bin
 
-set_path $RSFROOT/bin
 set_path $CWPROOT/bin
 set_path $SACROOT/bin
 set_path $SOFI2DROOT/bin $SOFI3DROOT/bin
@@ -37,27 +63,3 @@ set_path $HOME/.local/bin
 #         set_path "$dir"
 #     fi
 # done
-
-
-## Enable conda in command line
-set_conda_paths() {
-    local __conda_optpaths=(
-        "/opt/miniconda"
-        "/opt/conda"
-        "$OPTROOT/miniconda"
-        "$OPTROOT/miniconda3"
-        "$HOME/opt/miniconda3"
-    )
-    local conda_path
-    for conda_path in "${__conda_optpaths[@]}"; do
-        if [ -d "$conda_path" ]; then
-            [ -z "$_LOADED_CONDA_PATH" ] && source "$conda_path/etc/profile.d/conda.sh" 2>/dev/null && _LOADED_CONDA_PATH=1
-            break
-        fi
-    done
-}
-set_conda_paths
-
-[ -z "$_LOADED_RSF_ENV" ] && source $RSFROOT/share/madagascar/etc/env.sh 2>/dev/null && _LOADED_RSF_ENV=1
-
-# source /opt/intel/oneapi/setvars.sh >/dev/null 2>&1
