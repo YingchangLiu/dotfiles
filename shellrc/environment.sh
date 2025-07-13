15eb8975207db9fd7dfef2ceb51b9f1c55efc38f
kelen <58721349+YingchangLiu@users.noreply.github.com>
2025-07-13 14:57:13 +0800

    commit environments for fcitx

--- shellrc/environment.sh
+++ shellrc/environment.sh
@@ -21,10 +21,10 @@ export NNN_PLUG
 
 ## Input method
 export INPUT_METHOD=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
+#export GTK_IM_MODULE=fcitx
+#export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
-export SDL_IM_MODULE=fcitx
+#export SDL_IM_MODULE=fcitx
 export GLFW_IM_MODULE=fcitx
 export IMSETTINGS_MODULE=fcitx
 
