87c8754602f004aee28cc7449468ecc4c290edeb
office_wsl <58721349+YingchangLiu@users.noreply.github.com>
2026-01-29 00:00:10 +0800

    fix conda

--- config/conda/install_conda.sh
+++ config/conda/install_conda.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-_conda_file = "Miniconda3-latest-$(uname)-$(uname -m).sh"
-wget "https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/$(_conda_file)"
+#!/usr/bin/env bash
+_conda_file="Miniconda3-latest-$(uname)-$(uname -m).sh"
+wget "https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/$_conda_file"
 bash "$_conda_file"
 rm "$_conda_file"
